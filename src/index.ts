3d13fc4857a193d3708a54b2f0d3b784ee1f768d
Thibaud <thibaudgranier5@gmail.com>
2021-11-26 14:05:11 +0100

    fix: export decorators

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export default new Firestorm()
 export { Collection } from './Collection'
 export { SubCollection } from './Subcollection'
 export { CollectionRepository } from './CollectionRepository'
+export { field, date, subCollection } from './decorators'
