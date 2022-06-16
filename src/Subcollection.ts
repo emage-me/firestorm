1c3c86ceac1e5d3509fce37fc591c10bb2220202
Thibaud05 <thibaudgranier5@gmail.com>
2022-06-16 11:14:01 +0200

    feat: static subcollection query

--- src/Subcollection.ts
+++ src/Subcollection.ts
@@ -1,6 +1,7 @@
 import { CollectionRepository } from '.'
 import firestorm from './'
 import { Instance } from './Instance'
+import { FirestormQuery } from './Query'
 import { CollectionGroup, CollectionReference } from './types'
 
 export class SubCollection extends Instance {
@@ -12,6 +13,10 @@ export class SubCollection extends Instance {
 
   static collectionRef (): CollectionGroup {
     return firestorm.firestore.collectionGroup(this.collectionName)
+  }
+
+  static query<T extends typeof Instance> (this: T): FirestormQuery<T> {
+    return (new CollectionRepository<T>(this, null)).query
   }
 
   public static async findAllBy<T extends typeof Instance> (this: T, field: string, value: string | boolean): Promise<Array<InstanceType<T>>> { return await (new CollectionRepository<T>(this, null)).findAllBy(field, value) }
