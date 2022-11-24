e9eeed2df823d370e3417e9015c88978aef4f323
Thibaud05 <thibaudgranier5@gmail.com>
2022-11-24 10:38:44 +0100

    refactor: use singleton

--- src/Firestorm.ts
+++ src/Firestorm.ts
@@ -2,8 +2,15 @@ import { Firestore } from './types'
 
 export class Firestorm {
   firestore: Firestore
+  data: {}
+
+  constructor () {
+    this.data = {}
+  }
 
   initialize (firestore: Firestore): void {
     this.firestore = firestore
   }
 }
+
+export default new Firestorm()
--- src/moked/Firestorm.ts
+++ src/moked/Firestorm.ts
@@ -1,7 +1,16 @@
+import { Firestore } from '../types'
+
 export class Firestorm {
+  firestore: Firestore
   data: {}
 
   constructor () {
     this.data = {}
   }
+
+  initialize (firestore: Firestore): void {
+    this.firestore = firestore
+  }
 }
+
+export default new Firestorm()
